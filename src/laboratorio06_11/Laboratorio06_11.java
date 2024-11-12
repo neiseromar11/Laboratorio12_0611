31d449b71ef4e4e411a93b9e026d4fe6c9e7cb04
Neiser omar <49031435+neiseromar11@users.noreply.github.com>
2024-11-11 22:28:03 -0500

    F

---
+++ src/laboratorio06_11/Laboratorio06_11.java
@@ -0,0 +1,22 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio06_11;
+
+/**
+ *
+ * @author NEISER
+ */
+public class Laboratorio06_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Pantalla ventana= new Pantalla();
+         ventana.setLocationRelativeTo(null);
+         ventana.setVisible(true);
+    }
+    
+}
